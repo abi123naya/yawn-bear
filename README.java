73799959fb9ac71140648df401d970fe0a28b1a6
abi123naya <connect2abinaya@gmail.com>
2016-07-08 10:39:54 +0530

    Update and rename README.md to README.java
    finish

---
+++ README.java
@@ -0,0 +1,18 @@
+# yawn-bear
+import.java.util.*;
+class Check
+{ public static void main(String[] args) {
+int x;
+ Scaner s=new Scanner(System.in);
+ x=s.nextInt(); }
+ void decide()
+ {if(x==0)
+ {System.out.println("Zero"); }
+ else if(x>0)
+ { System.out.println("Positive");
+ }
+ else { System.out.println("Negaive");}
+ 
+ 
+
+}
